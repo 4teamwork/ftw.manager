464172ee3b7e8e1b2b44bafb59814b8077d1a38d
Jonas Baumann <j.baumann@4teamwork.ch>
2009-08-06 09:02:56 +0000

    fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/manager/version.txt').read().split()
+version = open('ftw/manager/version.txt').read().strip()
 
 setup(name='ftw.manager',
       version=version,
