026d37ee9df61c5fd73622bf0b55cbfa6bfe61a0
Jonas Baumann <j.baumann@4teamwork.ch>
2009-09-21 09:51:05 +0000

    added "i18nsync" command

--- ftw/manager/commands/i18ndude.py
+++ ftw/manager/commands/i18ndude.py
@@ -83,7 +83,7 @@ class BuildPotCommand(I18NDudeBaseCommand):
     """
 
     command_name = 'i18npot'
-    command_shortcut = 'lb'
+    command_shortcut = 'ib'
     description = 'Aktualisiert die i18n-POT-Dateien eines Packets'
 
     def __call__(self):
@@ -92,6 +92,7 @@ class BuildPotCommand(I18NDudeBaseCommand):
         package_root = scm.get_package_root_path('.')
         package_dir = os.path.join(package_root, *package_name.split('.'))
         pot_path = os.path.join(self.locales_dir, '%s.pot' % package_name)
+        output.part_title('Rebuilding pot file at %s' % pot_path)
         cmd = '%s rebuild-pot --pot %s --create %s %s' % (
                 self.i18ndude,
                 pot_path,
@@ -109,10 +110,47 @@ class SyncPoCommand(I18NDudeBaseCommand):
     oder aller Sprachen.
     """
     command_name = 'i18nsync'
-    command_shortcut = 'ls'
+    command_shortcut = 'is'
     description = 'Aktualisiert die Übersetzungs-Dateien einer Sprache'
+    usage = '%%prog %s [LANG-CODE]' % command_name
 
     def __call__(self):
-        raise NotImplemented
+        if len(self.args)<1:
+            output.error('Language code is required', exit=1)
+        lang = self.args[0]
+        # check
+        self.check_conditions()
+        package_name = scm.get_package_name('.')
+        # check pot file
+        pot_path = os.path.join(self.locales_dir, '%s.pot' % package_name)
+        if not os.path.exists(pot_path):
+            output.error('Could not find pot file at: %s' % pot_path, exit=1)
+        # check language directory
+        lang_dir = os.path.join(self.locales_dir, lang, 'LC_MESSAGES')
+        if not os.path.isdir(lang_dir):
+            runcmd('mkdir -p %s' % lang_dir)
+        # touch po file
+        po_file = os.path.join(lang_dir, '%s.po' % package_name)
+        if not os.path.isfile(po_file):
+            runcmd('touch %s' % po_file)
+        # sync
+        output.part_title('Syncing language "%s"' % lang)
+        cmd = '%s sync --pot %s %s' % (
+                self.i18ndude,
+                pot_path,
+                po_file,
+        )
+        runcmd(cmd)
+        # remove language
+        output.part_title('Removing language code from po file')
+        data = open(po_file).read().split('\n')
+        file = open(po_file, 'w')
+        for row in data:
+            if not row.startswith('"Language-Code') and not row.startswith('"Language-Name'):
+                file.write(row)
+                file.write('\n')
+        file.close()
+
+
 basecommand.registerCommand(SyncPoCommand)
 
