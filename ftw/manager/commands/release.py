bb47e37a16ea2a331df975accb4b9cece1beee0b
Jonas Baumann <j.baumann@4teamwork.ch>
2010-05-04 14:16:38 +0000

    Release: Added support for releasing from a branch

--- ftw/manager/commands/release.py
+++ ftw/manager/commands/release.py
@@ -1,18 +1,16 @@
 # -*- coding: utf8 -*-
 
+from ftw.manager.commands import basecommand
+from ftw.manager.utils import git
+from ftw.manager.utils import input
+from ftw.manager.utils import output
+from ftw.manager.utils import runcmd
+from ftw.manager.utils import runcmd_with_exitcode
+from ftw.manager.utils import scm
+from ftw.manager.utils import subversion as svn
+import ConfigParser
 import os
 import sys
-import ConfigParser
-
-from ftw.manager import utils
-from ftw.manager.commands import basecommand
-from ftw.manager.utils import *
-from ftw.manager.utils import output
-from ftw.manager.utils import input
-from ftw.manager.utils import subversion as svn
-from ftw.manager.utils import scm
-from ftw.manager.utils import git
-from ftw.manager.utils.memoize import memoize
 
 class ReleaseCommand(basecommand.BaseCommand):
     """
@@ -23,12 +21,12 @@ class ReleaseCommand(basecommand.BaseCommand):
     Bedingungen
 
     *   Die Option *long_description* im *setup.py* muss validierter restructuredText
-        sein
+    sein
     *   Man muss sich im Root-Verzeichnis eines SVN-Checkouts befinden: Wenn nur
-        das Egg erstellt (-e) wird, kann dies der trunk, ein branch oder ein tag
-        sein, ansonsten muss es der trunk sein.
+    das Egg erstellt (-e) wird, kann dies der trunk, ein branch oder ein tag
+    sein, ansonsten muss es der trunk sein.
     *   Das Projekt muss ein gültiges SVN-Layout haben, d.H. die Ordner trunk,
-        branches und tags besitzen
+    branches und tags besitzen
     *   Die Dateien setup.py und setup.cfg sind im aktuellen Ordner notwendig
     *   Die Version ist in der Datei my/package/version.txt gespeichert
     *   Eine Datei docs/HISTORY.txt ist notwendig
@@ -76,7 +74,7 @@ class ReleaseCommand(basecommand.BaseCommand):
         if self.options.release_egg:
             self.release_egg()
         if not self.options.release_egg_only:
-            self.switch_to_trunk()
+            self.switch_to_back()
         # swith back to trunk
 
     def analyse(self):
@@ -87,17 +85,20 @@ class ReleaseCommand(basecommand.BaseCommand):
                          exit=True)
         if scm.is_git('.'):
             git.pull_changes('.')
+            git.push_committed_changes('.')
         root_svn = scm.get_package_root_url('.')
-        if not svn.check_project_layout(root_svn, raise_exception=False):
+        if not svn.check_project_layout(root_svn, raise_exception=False,
+                                        ask_for_creation=False):
             # we should have the folders trunk, tags, branches in the project
             output.error('Project does not have default layout with trunk, ' +\
                              'tags and branches. At least one folder is missing.',
                          exit=True)
         if not self.options.release_egg_only:
-            if scm.get_svn_url('.')!=scm.get_package_root_url('.')+'/trunk':
+            if scm.get_svn_url('.') in (scm.get_package_root_url('.')+'/trunk',
+                                        scm.get_package_root_url('.')+'/branches',):
                 # command must be run at the "trunk" folder of a package
                 output.error('Please run this command at the root of the package' +\
-                                 '(trunk folder)', exit=True)
+                                 '(trunk/branch folder)', exit=True)
         if not os.path.isfile('setup.py'):
             # setup.py is required
             output.error('Could not find the file ./setup.py', exit=True)
@@ -129,6 +130,7 @@ class ReleaseCommand(basecommand.BaseCommand):
                     runcmd('git add MANIFEST.in')
                     runcmd('git commit -am "added MANIFEST.in for %s"' %
                            scm.get_package_name('.'))
+                    runcmd('git svn dcommit')
         # check subversion state
         if scm.has_local_changes('.'):
             output.error('You have local changes, please commit them first.',
@@ -224,7 +226,7 @@ class ReleaseCommand(basecommand.BaseCommand):
         output.part_title('Creating subversion tag')
         if scm.is_subversion('.'):
             root_url = scm.get_package_root_url('.')
-            trunk_url = os.path.join(root_url, 'trunk')
+            trunk_url = scm.get_svn_url('.')
             tag_url = os.path.join(root_url, 'tags', self.new_tag_version)
             cmd = 'svn cp %s %s -m "creating tag %s for package %s"' % (
                 trunk_url,
@@ -270,6 +272,8 @@ class ReleaseCommand(basecommand.BaseCommand):
             git.push_committed_changes('.')
 
     def bump_tag_after_tagging(self):
+        self._package_root = scm.get_package_root_url('.')
+        self._checkout_url = scm.get_svn_url('.')
         if scm.is_subversion('.'):
             tag_url = os.path.join(scm.get_package_root_url('.'), 'tags',
                                    self.new_tag_version)
@@ -318,15 +322,21 @@ class ReleaseCommand(basecommand.BaseCommand):
         runcmd(cmd)
         runcmd('rm -rf dist build')
 
-    def switch_to_trunk(self):
+    def switch_to_back(self):
         output.part_title('Cleaning up local repository')
         if scm.is_subversion('.'):
-            tag_url = os.path.join(scm.get_package_root_url('.'), 'trunk')
-            cmd = 'svn switch %s' % tag_url
+            cmd = 'svn switch %s' % self._checkout_url
             runcmd(cmd)
         elif scm.is_git('.'):
             git.push_committed_changes('.')
-            cmd = 'git checkout master'
+            folder = self._checkout_url[len(self._package_root)+1:]
+            if folder=='trunk':
+                folder = 'master'
+            elif folder.startswith('branches'):
+                folder = 'remotes/%s' % folder[len('branches/'):]
+            else:
+                folder = 'remotes/%s' % folder
+            cmd = 'git checkout %s' % folder
             cmd += ' ; git branch -D %s' % self.new_tag_version
             runcmd(cmd)
 
